11105abe6d02c187ab5c9ce5e329a32f7c233dce
Sharr15 <fogsliezeta@hotmail.com>
2020-12-02 21:08:27 +0100

    Added local git to github

---
+++ src/main/java/GamingLads/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package GamingLads.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080/%22")
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
